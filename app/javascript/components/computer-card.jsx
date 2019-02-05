3ae321412018db771e9a8ad8905c4abacca8b429
conejo11 <ec2-user@ip-172-31-11-94.us-east-2.compute.internal>
2019-02-05 17:25:22 +0000

    Card model DONE

--- app/javascript/components/computer-card.jsx
+++ app/javascript/components/computer-card.jsx
@@ -6,8 +6,34 @@ class List extends React.Component {
     return(
       <div className="card">
         <img src="lala.jpg" />
-         <h4><b>John Doe</b></h4>
-         <p>Architect & Engineer</p>
+        
+        <div className="card-conteiner">
+          <div className="card-text">
+            <h5 className="card-text-header"><b>NOME DO COMPUTADOR</b></h5>
+            <p className="card-text-content">Computador 1</p>
+          </div>
+          <div className="card-text">
+            <h5 className="card-text-header"><b>CATEGORIA</b></h5>
+            <p className="card-text-content">3D</p>
+          </div>
+        </div>
+        
+        <div className="card-text">
+          <h5 className="card-text-header"><b>CPU</b></h5>
+          <p className="card-text-content">17</p>
+        </div>
+        <div className="card-text">
+          <h5 className="card-text-header"><b>GPU</b></h5>
+          <p className="card-text-content">4</p>
+        </div>
+        <div className="card-text">
+          <h5 className="card-text-header"><b>RAM</b></h5>
+          <p className="card-text-content">33GB</p>
+        </div>
+        <div className="card-text">
+          <h5 className="card-text-header"><b>DISCO</b></h5>
+          <p className="card-text-content">481GB</p>
+        </div>
       </div>
     );
   }
